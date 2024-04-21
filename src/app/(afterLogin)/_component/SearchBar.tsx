cec8a966adc36a18ebd7e880c49abb08b6f50958
hwuiinn <gnldls8406@gmail.com>
2024-04-22 02:15:44 +0900

    Design : /explore 탐색하기 레이아웃 구성

--- src/app/(afterLogin)/_component/SearchBar.tsx
+++ src/app/(afterLogin)/_component/SearchBar.tsx
@@ -2,6 +2,7 @@
 import React, { useRef, useState } from "react";
 import styles from "./searchBar.module.css";
 import { usePathname } from "next/navigation";
+import SearchForm from "./SearchForm";
 
 const SearchBar = () => {
   const radioRef = useRef<HTMLInputElement>(null);
@@ -36,7 +37,6 @@ const SearchBar = () => {
                 ref={radioRef}
                 onChange={onChangeAll}
               />
-              <span className={styles.custom_radio_btn}></span>
             </div>
             <div className={styles.radio}>
               <label htmlFor="follower">내가 팔로우하는 사람들</label>
@@ -47,7 +47,6 @@ const SearchBar = () => {
                 value="on"
                 ref={radioRef}
               />
-              <span className={styles.custom_radio_btn}></span>
             </div>
           </div>
           <div>
@@ -59,6 +58,7 @@ const SearchBar = () => {
                 type="radio"
                 name="pf"
                 defaultChecked
+                ref={radioRef}
                 onChange={onChangeAll}
               />
             </div>
@@ -69,6 +69,7 @@ const SearchBar = () => {
                 type="radio"
                 name="pf"
                 value="on"
+                ref={radioRef}
                 onChange={onChangeFollow}
               />
             </div>
@@ -80,14 +81,7 @@ const SearchBar = () => {
 
   return (
     <div style={{ marginBottom: 60, width: "inherit" }}>
-      <form className={styles.search}>
-        <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
-          <g>
-            <path d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
-          </g>
-        </svg>
-        <input type="search" placeholder="Search" />
-      </form>
+      <SearchForm />
     </div>
   );
 };
--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styles from "./explore.module.css";
+import SearchForm from "../_component/SearchForm";
+import Trend from "../_component/Trend";
 
 const page = () => {
-  return <div>explore 페이지</div>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.formZone}>
+        <SearchForm />
+      </div>
+      <div className={styles.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 };
 
 export default page;
