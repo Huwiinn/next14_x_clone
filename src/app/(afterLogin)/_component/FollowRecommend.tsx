f679798d1e6064a2465001ccabf151d62d2217c2
hwuiinn <gnldls8406@gmail.com>
2024-06-13 18:30:42 +0900

    Feat : 팔로우 추천 => 팔로우 버튼 기능 적용 - Optimistic Update로 적용. (useMutation)

--- src/app/(afterLogin)/_component/FollowRecommend.tsx
+++ src/app/(afterLogin)/_component/FollowRecommend.tsx
@@ -1,23 +1,113 @@
 "use client";
 
 import style from "./followRecommend.module.css";
+import cx from "classnames";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { User } from "../../../model/User";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type Props = {
   recommendFollow: User;
 };
 
 export default function FollowRecommend({ recommendFollow }: Props) {
-  const session = useSession();
+  const { data: session } = useSession();
+  const queryClient = useQueryClient();
+  const followed = !!recommendFollow?.Followers.find(
+    (v) => v.userId === session?.user?.email
+  );
   const router = useRouter();
 
-  const onFollow = () => {
-    if (session.data === null) {
-      router.replace("/");
+  // const onFollow = () => {
+  //   if (session.data === null) {
+  //     router.replace("/");
+  //   } else {
+  //     alert("팔로우 완료!");
+  //   }
+  // };
+
+  const onFollow = useMutation({
+    mutationFn: () => {
+      return fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${recommendFollow.id}/follow`,
+        {
+          method: "POST",
+          credentials: "include",
+        }
+      );
+    },
+    onMutate() {
+      const value: User[] | undefined = queryClient.getQueryData([
+        "users",
+        "followRecommends",
+      ]);
+      if (value) {
+        const index = value.findIndex((v) => v.id === recommendFollow.id);
+        console.log(value, recommendFollow.id, index);
+        const shallow = [...value];
+        shallow[index] = {
+          ...shallow[index],
+          Followers: [{ userId: session?.user?.email as string }],
+          _count: {
+            ...shallow[index]._count,
+            Followers: shallow[index]._count?.Followers + 1,
+          },
+        };
+        queryClient.setQueryData(["users", "followRecommends"], shallow);
+      }
+    },
+    onError(err) {
+      console.log("팔로우 에러 : ", err);
+      console.error("팔로우 에러 : ", err);
+    },
+    onSettled() {},
+  });
+
+  const onUnFollow = useMutation({
+    mutationFn: () => {
+      return fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${recommendFollow.id}/follow`,
+        {
+          method: "POST",
+          credentials: "include",
+        }
+      );
+    },
+    onMutate() {
+      const value: User[] | undefined = queryClient.getQueryData([
+        "users",
+        "followRecommends",
+      ]);
+      if (value) {
+        const index = value.findIndex((v) => v.id === recommendFollow.id);
+        const shallow = [...value];
+        shallow[index] = {
+          ...shallow[index],
+          Followers: shallow[index].Followers.filter(
+            (v) => v.userId !== session?.user?.email
+          ),
+          _count: {
+            ...shallow[index]._count,
+            Followers: shallow[index]._count?.Followers - 1,
+          },
+        };
+        queryClient.setQueryData(["users", "followRecommends"], shallow);
+      }
+    },
+    onError(err) {
+      console.log("팔로우 에러 : ", err);
+      console.error("팔로우 에러 : ", err);
+    },
+    onSettled() {},
+  });
+
+  const onFollowHandler = () => {
+    console.log("팔로우 클릭!!");
+    if (followed) {
+      onUnFollow.mutate();
     } else {
-      alert("팔로우 완료!");
+      onFollow.mutate();
     }
   };
 
@@ -32,8 +122,11 @@ export default function FollowRecommend({ recommendFollow }: Props) {
         <div className={style.title}>{recommendFollow.nickname}</div>
         <div className={style.count}>@{recommendFollow.id}</div>
       </div>
-      <div className={style.followButtonSection}>
-        <button onClick={onFollow}>팔로우</button>
+      <div
+        className={cx(style.followButtonSection, followed && style.followed)}>
+        <button onClick={onFollowHandler}>
+          {followed ? "팔로잉" : "팔로우"}
+        </button>
       </div>
     </div>
   );
